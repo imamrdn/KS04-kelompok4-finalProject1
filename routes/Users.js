11f0c3d146602c6638bf65c279ce9cfb5b95da71
Imam Ramadhan <imamrmdn99@gmail.com>
2022-10-22 11:03:20 +0700

    fix : Routes directory

--- routes/Users.js
+++ routes/Users.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const ControllerUser = require('../controllers/UserController.js')
+const UserController = require('../controllers/UserController.js')
 
 router.post('/users/register', UserController.register)
 router.post('/users/login', UserController.login)
